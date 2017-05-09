9d0f4f5f723886619bce3738f5a348faa4ee88c7
diminator <dimi.dejonghe@gmail.com>
2017-05-09 19:12:36 +0200

    driver remove obsolote files

--- constants/application_constants.js
+++
@@ -1,8 +0,0 @@
-export const FLASK_BASE_URL = process.env.FLASK_BASE_URL;
-export const BDB_SERVER_URL = process.env.BDB_SERVER_URL;
-export const API_PATH = `${BDB_SERVER_URL}/api/v1/`;
-
-export default {
-    API_PATH,
-    FLASK_BASE_URL,
-};
