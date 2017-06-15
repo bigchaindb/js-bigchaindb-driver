5be86ce5c21c6649e11e0060fa04540c9c452631
vrde <agranzot@gmail.com>
2017-06-15 19:33:13 +0200

    Travis integration

--- .ci/travis-after-success.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e -x
-
-if [ "${TOXENV}" == "py35" ]; then
-    codecov
-fi
--- .ci/travis-before-script.sh
+++ .ci/travis-before-script.sh
@@ -2,11 +2,9 @@
 
 set -e -x
 
-if [ "${TOXENV}" == "py35" ]; then
-    rethinkdb --daemon
-    export BIGCHAINDB_KEYPAIR_PUBLIC=GW1nrdZm4mbVC8ePeiGWz6DqHexqewqy5teURVHi3RG4
-    export BIGCHAINDB_KEYPAIR_PRIVATE=2kQgBtQnHoauw8QchKM7xYvEBW1QDoHzhBsCL9Vi1AzB 
+rethinkdb --daemon
+export BIGCHAINDB_KEYPAIR_PUBLIC=GW1nrdZm4mbVC8ePeiGWz6DqHexqewqy5teURVHi3RG4
+export BIGCHAINDB_KEYPAIR_PRIVATE=2kQgBtQnHoauw8QchKM7xYvEBW1QDoHzhBsCL9Vi1AzB
 
-    # Start BigchainDB in the background and ignore any output
-    bigchaindb start >/dev/null 2>&1 &
-fi
+# Start BigchainDB in the background and ignore any output
+bigchaindb start >/dev/null 2>&1 &
