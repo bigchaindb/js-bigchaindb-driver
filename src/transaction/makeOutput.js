27d8dabd7a5d94c0dabfadaa1da082691a498f86
tim <tim@ascribe.io>
2017-06-16 11:43:09 +0200

    Stringify output.amount

--- src/transaction/makeOutput.js
+++ src/transaction/makeOutput.js
@@ -3,10 +3,13 @@
  * Create an Output from a Condition.
  * Note: Assumes the given Condition was generated from a single public key (e.g. a Ed25519 Condition)
  * @param {object} condition Condition (e.g. a Ed25519 Condition from `makeEd25519Condition()`)
- * @param {number} amount Amount of the output
+ * @param {string} amount Amount of the output
  * @returns {object} An Output usable in a Transaction
  */
-export default function makeOutput(condition, amount = 1) {
+export default function makeOutput(condition, amount = '1') {
+    if (typeof amount !== 'string') {
+        throw new TypeError('`amount` must be of type string')
+    }
     return {
         'amount': amount,
         condition,
