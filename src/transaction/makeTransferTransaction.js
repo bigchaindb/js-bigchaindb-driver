b8241589dff79198edbf4662c12577df5b16076e
tim <tim@ascribe.io>
2017-06-19 13:27:39 +0200

    fulfills.txid => fulfills.transaction_id + tests

--- src/transaction/makeTransferTransaction.js
+++ src/transaction/makeTransferTransaction.js
@@ -2,27 +2,9 @@ import makeInputTemplate from './makeInputTemplate'
 import makeTransaction from './makeTransaction'
 
 
-/**
- * @public
- * Generate a `TRANSFER` transaction holding the `asset`, `metadata`, and `outputs`, that fulfills
- * the `fulfilledOutputs` of `unspentTransaction`.
- * @param {object} unspentTransaction Previous Transaction you have control over (i.e. can fulfill
- *                                    its Output Condition)
- * @param {object} metadata Metadata for the Transaction
- * @param {object[]} outputs Array of Output objects to add to the Transaction.
- *                           Think of these as the recipients of the asset after the transaction.
- *                           For `TRANSFER` Transactions, this should usually just be a list of
- *                           Outputs wrapping Ed25519 Conditions generated from the public keys of
- *                           the recipients.
- * @param {...number} fulfilledOutputs Indices of the Outputs in `unspentTransaction` that this
- *                                     Transaction fulfills.
- *                                     Note that the public keys listed in the fulfilled Outputs
- *                                     must be used (and in the same order) to sign the Transaction
- *                                     (`signTransaction()`).
- * @returns {object} Unsigned transaction -- make sure to call signTransaction() on it before
- *                   sending it off!
- */
-export default function makeTransferTransaction(
+// TODO: Can we remove `export` here somehow, but still be able to import the
+// function for tests?
+export function _makeTransferTransaction(
         unspentTransaction,
         metadata,
         outputs,
@@ -43,5 +25,40 @@ export default function makeTransferTransaction(
                                                         : unspentTransaction.asset.id
     }
 
-    return makeTransaction('TRANSFER', assetLink, metadata, outputs, inputs)
+    return ['TRANSFER', assetLink, metadata, outputs, inputs]
+}
+
+/**
+ * @public
+ * Generate a `TRANSFER` transaction holding the `asset`, `metadata`, and `outputs`, that fulfills
+ * the `fulfilledOutputs` of `unspentTransaction`.
+ * @param {object} unspentTransaction Previous Transaction you have control over (i.e. can fulfill
+ *                                    its Output Condition)
+ * @param {object} metadata Metadata for the Transaction
+ * @param {object[]} outputs Array of Output objects to add to the Transaction.
+ *                           Think of these as the recipients of the asset after the transaction.
+ *                           For `TRANSFER` Transactions, this should usually just be a list of
+ *                           Outputs wrapping Ed25519 Conditions generated from the public keys of
+ *                           the recipients.
+ * @param {...number} fulfilledOutputs Indices of the Outputs in `unspentTransaction` that this
+ *                                     Transaction fulfills.
+ *                                     Note that the public keys listed in the fulfilled Outputs
+ *                                     must be used (and in the same order) to sign the Transaction
+ *                                     (`signTransaction()`).
+ * @returns {object} Unsigned transaction -- make sure to call signTransaction() on it before
+ *                   sending it off!
+ */
+
+// TODO:
+// - Make `metadata` optional argument
+// - Rename `fulfilledOutputs`, e.g. inputs
+// TODO: `outputs` should throw or include output in array if no array was
+// passed
+export default function makeTransferTransaction(
+        unspentTransaction,
+        metadata,
+        outputs,
+        ...fulfilledOutputs
+    ) {
+    return makeTransaction(..._makeTransferTransaction(...arguments))
 }
--- test/transaction/test_transaction.js
+++ test/transaction/test_transaction.js
@@ -1,5 +1,27 @@
 import test from 'ava'
-import { Transaction } from '../../src'
+import { Transaction, Ed25519Keypair } from '../../src'
+import { _makeTransferTransaction } from '../../src/transaction/makeTransferTransaction'
+import makeInputTemplate from '../../src/transaction/makeInputTemplate'
+
+
+// TODO: Find out if ava has something like conftest, if so put this there.
+const alice = new Ed25519Keypair()
+const aliceCondition = Transaction.makeEd25519Condition(alice.publicKey)
+const aliceOutput = Transaction.makeOutput(aliceCondition)
+const assetMessage = { assetMessage: 'assetMessage' }
+const metaDataMessage = { metaDataMessage: 'metaDataMessage' }
+const createTx = Transaction.makeCreateTransaction(
+    assetMessage,
+    metaDataMessage,
+    [aliceOutput],
+    alice.publicKey
+)
+const transferTx = Transaction.makeTransferTransaction(
+    createTx,
+    metaDataMessage,
+    [aliceOutput],
+    0
+)
 
 
 test('Create valid output with default amount', t => {
@@ -52,4 +74,48 @@ test('Pass condition not based on public_keys to makeOutput', t => {
 
 test('makeOutput throws TypeError with incorrect amount type', t => {
     t.throws(() => Transaction.makeOutput({}, 1337), TypeError)
+})
+
+
+test('Create TRANSFER transaction based on CREATE transaction', t => {
+    const testTx = _makeTransferTransaction(
+        createTx,
+        metaDataMessage,
+        [aliceOutput],
+        0
+    )
+    const expected = [
+        'TRANSFER',
+        {id: createTx.id },
+        metaDataMessage,
+        [aliceOutput],
+        [makeInputTemplate(
+            [alice.publicKey],
+            { output: 0, transaction_id: createTx.id }
+        )]
+    ]
+
+    t.deepEqual(testTx, expected)
+})
+
+
+test('Create TRANSFER transaction based on TRANSFER transaction', t => {
+    const testTx = _makeTransferTransaction(
+        transferTx,
+        metaDataMessage,
+        [aliceOutput],
+        0
+    )
+    const expected = [
+        'TRANSFER',
+        { id: transferTx.asset.id },
+        metaDataMessage,
+        [aliceOutput],
+        [makeInputTemplate(
+            [alice.publicKey],
+            { output: 0, transaction_id: transferTx.id }
+        )]
+    ]
+
+    t.deepEqual(testTx, expected)
 })
