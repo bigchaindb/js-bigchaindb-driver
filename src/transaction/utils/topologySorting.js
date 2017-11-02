d5591189eb04b91a7642f72034b2751a071baae5
michielmulders <michielmulders1@gmail.com>
2017-11-02 19:46:47 +0100

    Added Utility method for topological sort

---
+++ src/transaction/utils/topologySorting.js
@@ -0,0 +1,26 @@
+import toposort from 'toposort'
+
+function generateGraph(txs) {
+    const graph = []
+    for (var tx of txs) {
+        for (var input of tx.inputs) {
+            if (!!input.fulfills) {
+                graph.push([tx.id, input.fulfills.transaction_id])
+            }
+        }
+    }
+    return graph
+}
+
+
+/**
+ * @public
+ * Topological sort algorithm for transactions
+ * Used for endpoint: /transactions?assetId
+ * @param {object} txs Array of transactions
+ * @returns {object} Returns transactions in order of dependency
+ */
+export default function topsortTransactions(txs) {
+    const graph = generateGraph(txs)
+    return toposort(graph).reverse()
+}
