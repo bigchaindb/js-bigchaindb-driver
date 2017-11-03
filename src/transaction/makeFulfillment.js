5e838447dba0bf4ad25d4502eb1e28fdd680dca0
manolodewiner <manuel@bigchaindb.com>
2017-11-03 13:07:34 +0100

    create makeFulfillment

---
+++ src/transaction/makeFulfillment.js
@@ -0,0 +1,21 @@
+export default function createFulfillment(issuers = []) {
+    if (issuers.length === 1) {
+        const fulfillment = {
+            type: 'ed25519-sha-256',
+            public_keys: issuers
+        }
+        return fulfillment
+    } else {
+        const subcdts = []
+        issuers.map((issuer) => (
+            subcdts.push({ type: 'ed25519-sha-256',
+                public_key: issuer
+            })
+        ))
+        const fulfillment = {
+            type: 'threshold-sha-256',
+            subconditions: subcdts
+        }
+        return fulfillment
+    }
+}
