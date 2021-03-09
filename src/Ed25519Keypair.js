57a3e898715ef80d758a8db625f071809b60dfb2
getlarge <ed@getlarge.eu>
2021-03-09 07:51:36 +0100

    fix:  update polyfills path

--- src/Ed25519Keypair.js
+++ src/Ed25519Keypair.js
@@ -3,7 +3,7 @@
 // Code is Apache-2.0 and docs are CC-BY-4.0
 
 import base58 from 'bs58'
-import nacl from 'tweetnacl'
+import { sign } from 'tweetnacl'
 
 /**
  * @public
@@ -14,7 +14,7 @@ import nacl from 'tweetnacl'
  * @property {string} privateKey
  */
 export default function Ed25519Keypair(seed) {
-    const keyPair = seed ? nacl.sign.keyPair.fromSeed(seed) : nacl.sign.keyPair()
+    const keyPair = seed ? sign.keyPair.fromSeed(seed) : sign.keyPair()
     this.publicKey = base58.encode(Buffer.from(keyPair.publicKey))
     // tweetnacl's generated secret key is the secret key + public key (resulting in a 64-byte buffer)
     this.privateKey = base58.encode(Buffer.from(keyPair.secretKey.slice(0, 32)))
