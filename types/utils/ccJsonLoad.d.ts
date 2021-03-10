90a2cb2608c6c1bc82b3ecf287dc8c85bb4edafd
getlarge <ed@getlarge.eu>
2021-03-10 13:20:32 +0100

    fix: refine Crypoconditions parsers types

--- types/utils/ccJsonLoad.d.ts
+++ types/utils/ccJsonLoad.d.ts
@@ -2,10 +2,31 @@
 // SPDX-License-Identifier: (Apache-2.0 AND CC-BY-4.0)
 // Code is Apache-2.0 and docs are CC-BY-4.0
 
-import type { Condition, Fulfillment } from 'crypto-conditions';
-import type { JSONCondition } from './ccJsonify';
+import type {
+  Condition,
+  Ed25519Sha256,
+  PreimageSha256,
+  ThresholdSha256,
+} from 'crypto-conditions';
+import type {
+  Ed25519Sha256JSONCondition,
+  JSONCondition,
+  PreimageSha256JSONCondition,
+  ThresholdSha256JSONCondition,
+} from './ccJsonify';
 
-// TODO: improve returned type accuracy
-export default function ccJsonLoad<T = TypeId.Ed25519Sha256>(
-  conditionJson: JSONCondition[T]
-): Condition | Fulfillment;
+declare function ccJsonLoad(
+  conditionJson: PreimageSha256JSONCondition
+): PreimageSha256;
+
+declare function ccJsonLoad(
+  conditionJson: ThresholdSha256JSONCondition
+): ThresholdSha256;
+
+declare function ccJsonLoad(
+  conditionJson: Ed25519Sha256JSONCondition
+): Ed25519Sha256;
+
+declare function ccJsonLoad(conditionJson: JSONCondition): Condition;
+
+export default ccJsonLoad;
