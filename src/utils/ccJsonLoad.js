2a104eb86bb7efc2c400e38a7637aed94cd7f795
getlarge <ed@getlarge.eu>
2021-03-10 13:36:00 +0100

    fix: Crypto conditions parsers

--- src/utils/ccJsonLoad.js
+++ src/utils/ccJsonLoad.js
@@ -2,9 +2,8 @@
 // SPDX-License-Identifier: (Apache-2.0 AND CC-BY-4.0)
 // Code is Apache-2.0 and docs are CC-BY-4.0
 
-import { Buffer } from 'buffer'
 import base58 from 'bs58'
-import cc from 'crypto-conditions'
+import { Condition, Ed25519Sha256, ThresholdSha256 } from 'crypto-conditions'
 
 /**
  * Loads a crypto-condition class (Fulfillment or Condition) from a BigchainDB JSON object
@@ -13,17 +12,18 @@ import cc from 'crypto-conditions'
  */
 export default function ccJsonLoad(conditionJson) {
     if ('hash' in conditionJson) {
-        const condition = new cc.Condition()
-        condition.type = conditionJson.type_id
-        condition.bitmask = conditionJson.bitmask
-        condition.hash = Buffer.from(base58.decode(conditionJson.hash))
+        const condition = new Condition()
+        condition.setTypeId(conditionJson.type_id)
+        condition.setSubtypes(conditionJson.bitmask)
+        condition.setHash(base58.decode(conditionJson.hash))
+        // TODO: fix this, maxFulfillmentLength cannot be set in CryptoCondition lib
         condition.maxFulfillmentLength = parseInt(conditionJson.max_fulfillment_length, 10)
         return condition
     } else {
         let fulfillment
 
         if (conditionJson.type === 'threshold-sha-256') {
-            fulfillment = new cc.ThresholdSha256()
+            fulfillment = new ThresholdSha256()
             fulfillment.threshold = conditionJson.threshold
             conditionJson.subconditions.forEach((subconditionJson) => {
                 const subcondition = ccJsonLoad(subconditionJson)
@@ -36,8 +36,8 @@ export default function ccJsonLoad(conditionJson) {
         }
 
         if (conditionJson.type === 'ed25519-sha-256') {
-            fulfillment = new cc.Ed25519Sha256()
-            fulfillment.publicKey = Buffer.from(base58.decode(conditionJson.public_key))
+            fulfillment = new Ed25519Sha256()
+            fulfillment.setPublicKey(base58.decode(conditionJson.public_key))
         }
         return fulfillment
     }
